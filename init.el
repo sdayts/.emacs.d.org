923f4de60e1ce841f3315623098ae4876414e36a
Sergey Dayts <sdayts@gmail.com>
2018-01-17 15:46:39 -0500

    Org mode, rect mode fixes. Copy on mouse selection.

--- init.el
+++ init.el
@@ -1,7 +1,15 @@
+(if (file-readable-p "~/.emacs.d/before-init.el")
+      (load-file "~/.emacs.d/before-init.el"))
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+	     '("gnu" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives
+	     '("org" . "https://orgmode.org/elpa/"))
+
 (package-initialize)
 
 ;; Bootstrap `use-package'
@@ -15,7 +23,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (whole-line-or-region helm use-package))))
+ '(package-selected-packages
+   (quote
+    (org dired+ org-mode whole-line-or-region helm use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
