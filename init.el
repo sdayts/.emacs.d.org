c90788226e01d25a554aa9e7aa2e2fa94e60ccaf
Sergey Dayts <sdayts@gmail.com>
2018-02-01 20:41:04 -0500

    Added doxymacs.

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 (if (file-readable-p "~/.emacs.d/before-init.el")
-      (load-file "~/.emacs.d/before-init.el"))
+    (load-file "~/.emacs.d/before-init.el"))
 
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -21,7 +21,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ace-jump-face-foreground ((t (:inherit ace-jump-face-foreground :height 1.0 :foreground "yellow" :background "black")))))
+ '(ace-jump-face-foreground ((t (:inherit ace-jump-face-foreground :height 1.0 :foreground "yellow" :background "black"))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 6.0)))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -29,4 +30,4 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (org shell-pop zoom-window yasnippet wrap-region whole-line-or-region which-key try transpose-frame swap-buffers rtags multiple-cursors move-text magit helm-projectile helm expand-region iedit dired+ dired-narrow dired-hacks-utils beacon back-button auto-complete ace-jump-mode use-package))))
+    (modern-cpp-font-lock ivy helm-grep vlf ace-window org shell-pop zoom-window yasnippet wrap-region whole-line-or-region which-key try transpose-frame swap-buffers rtags multiple-cursors move-text magit helm-projectile helm expand-region iedit dired+ dired-narrow dired-hacks-utils beacon back-button auto-complete ace-jump-mode use-package))))
